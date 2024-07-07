8c099e08d4cf2830a76b29e580ceb9df75ede0e4
mck0324 <dch3030@naver.com>
2024-07-07 22:06:32 +0900

    useEffect uses

--- react-beginner/src/App.js
+++ react-beginner/src/App.js
@@ -4,13 +4,25 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the thime");
+  const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
-    console.log("Call The API...");
+    console.log("i run only once");
   },[]);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  },[keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  },[counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword'&'counter' changes");
+  },[keyword,counter]);
+
   return (
     <div className="App">
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here...." />
       <h1 className={styles.title}>Welcome back!!</h1>
       <h1>{counter}</h1>
       <button onClick={onClick}>Click</button>

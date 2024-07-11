828743d4e5accd794013db281a2f3b539a385c3d
mck0324 <dch3030@naver.com>
2024-07-11 14:57:10 +0900

    Movie title가져오기

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -3,13 +3,18 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
-    .then((response) => response.json())
-    .then((json) => setMovies(json.data.movies));
+    getMovies()
   },[]);
   console.log(movies);
-  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
+  return <div>{loading ? <h1>Loading...</h1> : <div>{movies.map(movie => <div key={movie.id}>{movie.title}</div>)}</div>}</div>;
 }
 
 export default App;

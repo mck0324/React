564408940705fe7cf9c85792aa33ea8b4193d406
mck0324 <dch3030@naver.com>
2024-07-15 23:39:40 +0900

    똑같은걸  기본의 방식(Class Component)으로 만들어보기

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,18 +1,52 @@
 import { useState } from 'react';
 import './App.css';
 
-function App() {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decreamentItem = () => setItem(item - 1);
-  return (
-    <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>Start</h2>
-      <button onClick={incrementItem} >Increment</button>
-      <button onClick={decreamentItem}>Decrement</button>
-    </div>
-  );
+// function App() {
+//   const [item, setItem] = useState(1);
+//   const incrementItem = () => setItem(item + 1);
+//   const decreamentItem = () => setItem(item - 1);
+//   return (
+//     <div className="App">
+//       <h1>Hello {item}</h1>
+//       <h2>Start</h2>
+//       <button onClick={incrementItem} >Increment</button>
+//       <button onClick={decreamentItem}>Decrement</button>
+//     </div>
+//   );
+// }
+
+
+// 똑같은걸 기존의 방식(Class Component)으로 만들어보기
+class AppUgly extends React.Component{
+  state = {
+    item : 1
+  }
+  render(){
+    const {item} = this.state;
+    return (
+      <div>
+        <h1>Hello {item}</h1>
+        <h2>Start</h2>
+        <button onClick={this.incrementItem} >Increment</button>
+        <button onClick={this.decreamentItem}>Decrement</button>
+      </div>
+    );
+  }
+  incrementItem = () => {
+    this.setState(state => {
+      return {
+        item: state.item + 1
+      };
+    });
+  };
+  decreamentItem = () => {
+    this.setState(state => {
+      return {
+        item: state.item - 1
+      };
+    });
+  };
 }
 
-export default App;
+// export default App;
+

9b1a4c35af1c8419c09a11c48ad53d28000b4527
mck0324 <dch3030@naver.com>
2024-07-22 17:55:11 +0900

    React Hooks(useConfirm)

--- react-hooks/src/routes/useEffect4.js
+++ react-hooks/src/routes/useEffect4.js
@@ -1,5 +1,5 @@
 const useConfirm = (message="", onConfirm, onCancel) => {
-    if (onConfirm && typeof onConfirm !== "function") {
+    if (!onConfirm || typeof onConfirm !== "function") {
         return;
     }
     if (onCancel && typeof onCancel !== "function") {

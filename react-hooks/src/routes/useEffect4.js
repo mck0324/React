c3a313f14db57c4f0233afc77bd2549bbf7ae4d7
mck0324 <dch3030@naver.com>
2024-07-22 17:54:31 +0900

    React Hooks(useConfirm)

--- react-hooks/src/routes/useEffect4.js
+++ react-hooks/src/routes/useEffect4.js
@@ -1,8 +1,26 @@
+const useConfirm = (message="", onConfirm, onCancel) => {
+    if (onConfirm && typeof onConfirm !== "function") {
+        return;
+    }
+    if (onCancel && typeof onCancel !== "function") {
+        return;
+    }
+    const confirmAction = () => {
+        if (window.confirm(message)) {
+            onConfirm();
+        } else {
+            onCancel();
+        }
+    }
+    return confirmAction;
+}
 function Hook8() {
-
+    const deleteWorld = () => console.log("Deleting the world");
+    const abort = () => console.log("Aborted");
+    const confirmDelete = useConfirm("Are you sure",deleteWorld, abort);
     return (
         <div className="App">
-            <h1>Hi</h1>
+            <button onClick={confirmDelete}>Delete the world</button>
         </div>
     )
 }
